e4ca0e57db71d4c2412c0d7ecc28acb3f7b04862
Rodri-Zelaya <rzelayasalinas7@gmail.com>
2025-05-24 17:04:18 -0600

    Implementacion de btnCancelar

--- EntidadFinanciera2M6/obj/Debug/net8.0-windows/EntidadFinanciera2M6.AssemblyInfo.cs
+++ EntidadFinanciera2M6/obj/Debug/net8.0-windows/EntidadFinanciera2M6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2ff7b3a118f90dc33a4f38e43d2cd6dba0c4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5283fe8571b7f7f1ff7e2f81916167a35596614")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
