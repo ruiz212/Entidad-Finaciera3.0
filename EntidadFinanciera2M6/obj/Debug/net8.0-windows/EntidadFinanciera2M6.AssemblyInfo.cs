902e0a53650d52bbdfc28a090b1845752ee02f4b
Jenniffer Ramirez <jennifferramirezb@gmail.com>
2025-05-24 09:40:24 -0600

    implementacion de metodos DesactivarCeunta, Transferir fondos y Obtener Transacciones en la clase EFService

--- EntidadFinanciera2M6/obj/Debug/net8.0-windows/EntidadFinanciera2M6.AssemblyInfo.cs
+++ EntidadFinanciera2M6/obj/Debug/net8.0-windows/EntidadFinanciera2M6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e9946e537bd8dc4e1a7603eb911728adc61561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a4030c6096dfbf4cc2fbbadefbcced66c8871d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntidadFinanciera2M6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
